dcf4ee6b0b3c987566c867f6722d080d06af3192
Randika <64809687+DonRanS@users.noreply.github.com>
2022-05-24 10:08:42 +1200

    x

--- js/header.js
+++ js/header.js
@@ -11,11 +11,11 @@ document.write('<header> <div class="navbar navbar-expand-lg bg-dark navbar-dark
     '                \n' +
     '                <div class="navbar-nav ml-auto" >\n' +
     '                    <!-- navbar link -->\n' +
-    '                    <a href="./Menu.html"  class="nav-item nav-link active">MANU</a>\n' +
-    '                    <a href="./Booking.html"  class="nav-item nav-link active">BOOKING</a>\n' +
-    '                    <a href="./Aboutus.html"  class="nav-item nav-link active">ABOUT US</a>\n' +
-    '                    <a href="./Contactus.html" class="nav-item nav-link active">CONTACT US</a>\n' +
-    '                    <a href="./Gallery.html"  class="nav-item nav-link active">Gallery</a>\n' +
+    '                    <a href="../Menu.html"  class="nav-item nav-link active">MANU</a>\n' +
+    '                    <a href="../Booking.html"  class="nav-item nav-link active">BOOKING</a>\n' +
+    '                    <a href="../Aboutus.html"  class="nav-item nav-link active">ABOUT US</a>\n' +
+    '                    <a href="../Contactus.html" class="nav-item nav-link active">CONTACT US</a>\n' +
+    '                    <a href="../Gallery.html"  class="nav-item nav-link active">Gallery</a>\n' +
     '                </div>\n' +
     '            </div>    \n' +
     '        </div>\n' +
